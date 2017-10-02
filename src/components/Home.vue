6daaf2113ce73fbb92ab124c6ea947af8e80893a
Oxyrus <im.oxyrus@gmail.com>
2017-10-02 10:05:03 -0500

    Setup home page structure

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+
+    <header>
+      <h1>Idea Nebulae</h1>
+      <p>where ideas are born</p>
+      <button>start ideation</button>
+    </header>
+
+    <section id="features">
+      <h2>You can...</h2>
+      <div>
+        <img src="../assets/people.svg" />
+        <p>Get feedback from a helpful community</p>
+      </div>
+      <div>
+        <img src="../assets/bulb.svg" />
+        <p>Donate for someone to make it reality</p>
+      </div>
+      <div>
+        <img src="../assets/lock.svg" />
+        <p>Be assured your idea will not get stolen</p>
+      </div>
+    </section>
+
+    <section id="idea-count">
+      <h2>Ideas posted till now... <strong>12345</strong></h2>
+      <button>start ideation</button>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+};
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/shared/MainNavbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <ul>
+      <li>Login</li>
+      <li>Register</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MainNavbar',
+};
+</script>
+
+<style language="stylus" scoped>
+
+</style>
