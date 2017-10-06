e35ca857d427ed997e3dc625e5f3068a541fa39a
Oxyrus <im.oxyrus@gmail.com>
2017-10-06 13:10:42 -0500

    Create components for the different routes

---
+++ src/components/ideas/IdeaDetails.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>IdeaDetails</h1>
+</template>
+
+<script>
+export default {
+  name: 'IdeaDetails',
+};
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/users/UserProfile.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Profile component</h1>
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+};
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
