12346cad8962cf35cd838c5f1b8605b7b145df8f
Sarah Schneider <rifkegribenes@gmail.com>
2017-10-16 21:34:01 -0700

    class names and basic styles

--- client/src/components/ideas/CreateIdea.vue
+++ client/src/components/ideas/CreateIdea.vue
@@ -3,82 +3,79 @@
     <header class="create__header">
       <h1 class="create__title">Create Idea</h1>
     </header>
-    
-    <section class="create__form__wrapper">
-      <div class="create__form__group">
-        
-        <div class="createidea__form__element">
-          <label for="createidea__title">Title</label>
-          <input id="createidea__title" maxlength="100" type="text" name="title" v-model="ideaTitle" autofocus>
+
+    <section class="create__form-wrapper">
+      <div class="create__form-group">
+
+        <div class="create__form-element">
+          <label class="create__label" for="create__title">Title</label>
+          <input class="create__input" id="create__title" maxlength="100" type="text" name="title" v-model="ideaTitle" placeholder="Title" autofocus>
         </div>
 
-        <div class="createidea__form__element">
-          <label for="createidea__desc">Description</label>
-          <textarea id="createidea__desc" type="text" name="description" cols="80" rows="13" maxlength="1000" v-model="ideaDesc"></textarea>
+        <div class="create__form-element">
+          <label class="create__label" for="create__desc">Description</label>
+          <textarea id="create__desc" name="description" class="create__textarea" cols="80" rows="13" maxlength="1000" v-model="ideaDesc" placeholder="Description" ></textarea>
         </div>
 
-        <div class="createidea__form__tags">
-          <label for="createidea__tags">Tags</label>
-          <div class="createidea__info" v-if="tags.length == 0">No tags added yet</div>
-          <div id="createidea__tags" class="create__form__tag" v-for="(tag, index) in tags">
-            <div class="createidea__link">
+        <div class="create__form-tags">
+          <div id="create__tags" class="create__form-tag" v-for="(tag, index) in tags">
+            <div class="create__link">
               <div class="tag">{{tag}}</div>
               <button id="remove__link" @click="removeTag(index)"> &#10006; </button>
             </div>
           </div>
 
-          <div class="createidea__addtag">
-            <label for="newTag">Add Tag</label>
-            <input name="newTag" type="text" v-model="tagText" @keyup.enter="addTag">
-            <button class="createidea__addlink__button"
+          <div class="create__addtag">
+            <label class="create__label" for="newTag">Add Tag</label>
+            <input class="create__input" name="newTag" type="text" v-model="tagText" @keyup.enter="addTag">
+            <button class="create__addlink-button"
             @click="addTag"> + </button>
           </div>
         </div>
 
-        <div class="createidea__form__element">
-          <label for="createidea__links">Links to more info</label>
-          <div class="createidea__info" v-if="links.length == 0">No links added yet</div>
-          <div id="createidea__links" class="create__form__link" v-for="(link, index) in links">
-            <div class="createidea__link">
+        <div class="create__form-element">
+          <label class="create__label" for="create__links">Links to more info</label>
+          <div class="create__info" v-if="links.length == 0">No links added yet</div>
+          <div id="create__links" class="create__form__link" v-for="(link, index) in links">
+            <div class="create__link">
               <a :href="link">{{link}}</a>
               <button id="remove__link" @click="removeLink(index)"> &#10006; </button>
             </div>
           </div>
-          
-          <div class="createidea__addlink">
+
+          <div class="create__addlink">
             <div v-show="errors.has('newlink')">Invalid link</div>
-            <label for="newlink">Add link</label>
-            <input name="newlink" v-validate="'required|url'" data-vv-delay="1000" type="text" v-model="linkText" @keyup.enter="addLink">
-            <button class="createidea__addlink__button"
+            <label class="create__label" for="newlink">Add link</label>
+            <input class="create__input" name="newlink" v-validate="'required|url'" data-vv-delay="1000" type="text" v-model="linkText" @keyup.enter="addLink">
+            <button class="create__addlink-button"
             @click="addLink"> + </button>
           </div>
         </div>
 
-        <div class="createidea__form__element">
-          <label for="createidea__type">Type</label>
-          <div id="createidea__type" @mouseover="upHere = 0" @mouseleave="upHere = -1">
-            <input type="radio" name="ideatype" v-validate="'required'" value="0" v-model="ideaType">
-            <div class="type__title">Public</div>
-            <div class="type__desc" v-if="upHere == 0">Anyone can read and give feedback</div>
-          </div>
-          <div id="createidea__type" @mouseover="upHere = 1" @mouseleave="upHere = -1" >
-            <input type="radio" name="ideatype" value="1" v-model="ideaType">
-            <div class="type__title">Private</div>
-            <div class="type__desc" v-if="upHere == 1">Only visible to people that agree to the license</div>
-          </div>
-          <div id="createidea__type" @mouseover="upHere = 2" @mouseleave="upHere = -1" >
-            <input type="radio" name="ideatype" value="2" v-model="ideaType">
-            <div class="type__title">Custom</div>
-            <div class="type__desc" v-if="upHere == 2">Customise the license and choose the people that see the idea</div>
+        <div class="create__form-element">
+          <label class="create__label" for="create__type">Type</label>
+          <div class="create__radio-group">
+            <div class="create__type" @mouseover="upHere = 0" @mouseleave="upHere = -1">
+              <input type="radio" name="ideatype" v-validate="'required'" value="0" v-model="ideaType">
+              <div class="create__type-title">Public</div>
+              <div class="create__type-desc" v-if="upHere == 0">Anyone can read and give feedback</div>
+            </div>
+            <div class="create__type" @mouseover="upHere = 1" @mouseleave="upHere = -1" >
+              <input type="radio" name="ideatype" value="1" v-model="ideaType">
+              <div class="create__type-title">Private</div>
+              <div class="create__type-desc" v-if="upHere == 1">Only visible to people who agree to the license</div>
+            </div>
+            <div class="create__type" @mouseover="upHere = 2" @mouseleave="upHere = -1" >
+              <input type="radio" name="ideatype" value="2" v-model="ideaType">
+              <div class="create__type-title">Custom</div>
+              <div class="create__type-desc" v-if="upHere == 2">Customise the license and choose the people that see the idea</div>
+            </div>
           </div>
         </div>
       </div>
-
-      <div class="createidea__cancel__button">
-        <a href="/dashboard">Cancel</a>
-      </div>
-      <div class="createidea__submit__button">
-        <button @click="submitIdea">Submit</button>
+      <div class="create__button-wrap">
+        <a class="btn btn__primary profile__button create__button--btm" href="/dashboard">Cancel</a>
+        <button class="btn btn__primary profile__button create__button--btm" @click="submitIdea">Submit</button>
       </div>
     </section>
   </div>
@@ -132,16 +129,85 @@ export default {
       this.tags.splice(index, 1);
     },
     submitIdea() {
-      const payload = {
-        ideaTitle: this.ideaTitle,
-        ideaDesc: this.ideaDesc,
-        links: this.links,
-      };
+      // const payload = {
+      //   ideaTitle: this.ideaTitle,
+      //   ideaDesc: this.ideaDesc,
+      //   links: this.links,
+      // };
     },
   },
 };
 </script>
 
 <style lang="stylus" scoped>
+@import '~stylus_var'
+
+.create
+
+  &__form-wrapper
+    border 1px solid $purple
+    padding 40px
+    width 100%
+    max-width 600px
+    margin auto
+
+  &__header
+    text-align center
+    color $purple
+    margin-bottom 60px
+    font-size 1.5em
+    line-height 1.2em
+      @media (min-width: 600px)
+        font-size 36px
+
+    & h1
+      font-weight 200 !important
+
+  &__label
+    text-transform uppercase
+    font-size .8em
+    letter-spacing 2.5px
+    font-weight 700
+    display block
+
+  &__input
+    padding 10px
+    width 100%
+    margin-bottom 10px
+    font-size 1em
+    border 1px solid $purple
+
+  &__textarea
+    width 100%
+    padding 10px
+    font-size 1em
+    font-family: 'Titillium Web', Helvetica, Arial, sans-serif
+    color: $gray_text
+    letter-spacing: 1px
+    line-height: 1.5em
+    border 1px solid $purple
+    margin-bottom 10px
+
+  &__type
+    display inline-block
+    width 33%
+
+  &__radio-group
+    display flex
+    width 100%
+
+  &__button-wrap
+    display flex
+    justify-content center
+    margin 20px auto
+
+  &__button
+
+    &--btm
+      margin-top 1vh
+      text-decoration none
+
+      &:last-child
+        margin-left 20px
 
 </style>
