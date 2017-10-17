4ed3e7b07be55643d92a5d82f05da8411de246d0
jdmedlock <jdmedlock@gmail.com>
2017-10-17 16:22:50 -0500

    Add User DB schema

---
+++ models/users.js
@@ -0,0 +1,17 @@
+// grab the things we need
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create the schema for the User collection
+const userSchema = new Schema({
+  user_id: { type: String, required: true, unique: true },
+  user_name: { type: String, required: true, unique: false },
+  security_id: { type: String, required: true, unique: false },
+  avatar_url: { type: String, required: true, unique: false },
+  qualifications: { type: String, required: false, unique: false },
+});
+
+// Create a model for the schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
