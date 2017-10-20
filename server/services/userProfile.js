a241f4c95c98fd0d78b8533a825b56ba2f1c465d
jdmedlock <jdmedlock@gmail.com>
2017-10-20 15:53:54 -0500

    Add user profile CRUD API [DO NOT USE]

--- server/services/userProfile.js
+++ server/services/userProfile.js
@@ -5,14 +5,55 @@ import Users from '../models/Users';
 /**
  * User Service
  *
- * This module contains functions supporting API access to the User information in the database.
+ * This module contains functions supporting API access to the User information
+ * in the database.
  */
 
 /**
+ * @description Add the profile information for a specified user.
+ * @param {Object} userProfile - An object containing the users profile
+ * information that is to be added.
+ * @returns {Promise} promise - A Promise that when resolved will contain 
+ * information about the final state of the request. */
+const addUserProfile = (userProfile) => {
+  // TODO: Complete this function
+};
+
+/**
+ * @description Delete the profile information for a specified user.
+ * @param {String} userId - User id specifying which users information is to be
+ * deleted.
+ * @returns {Object} promise - A promise which when resolved will contain
+ * information about the final state of the request.
+ */
+const deleteUserProfile = (userId) => {
+  mongoose.Promise = global.Promise;
+  const promise = new Promise((resolve, reject) => {
+    mongoose.connect(config.db.mongoURI, {useMongoClient: true,})
+    .then(() => {
+      User.findOneAndRemove({ username: `${userId}` }, function(err) {
+        const requestState = { 
+          action: 'deleteUserProfile',
+          parm: userId,
+          finalState: 'success',
+        }
+        requestState.finalState = err ? 'failed' : 'success';
+        resolve(requestState); 
+      })       
+    })
+    .catch((error) => {
+      reject(`UserProfile.deleteUserProfile: Mongoose connect failure - ${error}`);
+    });
+  });
+  return promise;
+}; 
+
+/**
  * @description Retrieve the information for a specified user.
- * @param {String} userId - User id specifying which users information is to be retrieved.
- * @returns {Promise} promise - A Promise that when resolved will contain the User Profile 
- * JSON object..
+ * @param {String} userId - User id specifying which users information is to be
+ * retrieved.
+ * @returns {Promise} promise - A Promise that when resolved will contain the
+ * User Profile JSON object.
  */
 const getUserProfile = (userId) => {
   mongoose.Promise = global.Promise;
@@ -33,4 +74,15 @@ const getUserProfile = (userId) => {
   return promise;
 }; 
 
-export { getUserProfile };
+/**
+ * @description Update the profile information for a specified user.
+ * @param {Object} userProfile - An object containing the users profile
+ * information that is to be updated.
+ * @returns {Promise} promise - A Promise that when resolved will contain 
+ * information about the final state of the request.
+ */
+const updateUserProfile = (userProfile) => {
+  // TODO: Complete this function
+};
+
+export { addUserProfile, deleteUserProfile, getUserProfile, updateUserProfile };
