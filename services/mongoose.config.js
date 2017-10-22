35008733fbead6b073a52e2004042d9c0168f705
Oxyrus <im.oxyrus@gmail.com>
2017-10-22 13:28:25 -0500

    Go back to proccess variables

--- services/mongoose.config.js
+++ services/mongoose.config.js
@@ -1,7 +1,7 @@
 let config = {};
 config.db = {};
 
-config.db.host = `IdeaNebulae:Columbia*India1@ds011732.mlab.com:11732`;
+config.db.host = `${process.env.DBUSERID}*${process.env.DBPASSWD}.mlab.com:11732`;
 config.db.name = 'ideanebulae';
 config.db.mongoURI = `mongodb://${config.db.host}/${config.db.name}`;
 
