d98047766da3d8d3a9406a478001110b23aa89dd
jdmedlock <jdmedlock@gmail.com>
2017-10-24 21:14:05 -0500

    Update createOrUpdateUser function to update qualifications

--- models/methods/userMethods.js
+++ models/methods/userMethods.js
@@ -6,19 +6,19 @@ export default class userMethods {
 
   // Finds an user using the id
   static async findUser(userId) {
-    console.log(`Made it to findUser - userId:${userId}`);
     return await this.findOne({'user_id': userId});
   }
 
   // Update a user if it exists, otherwise inserts it
-  static async createOrUpdateUser(userProfile) {
-    console.log(`Made it to createOrUpdateUser. userProfile.userId: ${userProfile.userId}`);
-    console.log(userProfile);
+  static async createOrUpdateUser(currId, profileData) {
+    console.log(`Made it to createOrUpdateUser.\n...currId: ${currId}\n...profileData: ${profileData}`);
+    let user = new this();
+    user.qualifications = profileData.qualifications;
+    delete this._id;
     return await this.findOneAndUpdate(
-      {'user_id': userProfile.userId},
-      userProfile,
-      { upsert: true }
-    );
+      { user_id: currId },
+      user,
+      { upsert: false, new: true, runValidators: true }
+    ).exec();
   }
-  
 }
