5d2a3fd48469583968b01df8528fed34e22c3894
Sarah Schneider <rifkegribenes@gmail.com>
2017-10-29 11:56:15 -0700

    added avatar in headers nav for logged in user

--- client/src/components/shared/MainNavbar.vue
+++ client/src/components/shared/MainNavbar.vue
@@ -1,7 +1,21 @@
 <template>
   <nav class="nav">
     <div class="nav__menu">
-      <button class="btn btn__primary splash__button nav__item" v-show="isLoggedIn()">{{ loggedUserNickname }}</button>
+      <a
+        :href="'/profile/' + loggedUserNickname"
+        class="btn nav__avatar"
+        v-show="isLoggedIn()">
+        <div class="nav__image-aspect">
+          <div class="nav__image-crop">
+            <div
+              class="nav__image"
+              :style="{ backgroundImage: `url(${loggedUserAvatarUrl})`, backgroundSize: 'cover', backgroundPosition: 'center center', }"
+              role="image"
+              :aria-label="loggedUserNickname"
+              />
+          </div>
+        </div>
+      </a>
       <button class="btn btn__primary splash__button nav__item" v-show="!isLoggedIn()" @click="handleLogin()">Login</button>
       <button class="btn btn__primary splash__button nav__item" v-show="isLoggedIn()" @click="handleLogout()">Logout</button>
     </div>
@@ -17,6 +31,9 @@ export default {
   computed: {
     loggedUserNickname() {
       return this.$store.state.profile.nickname;
+    },
+    loggedUserAvatarUrl() {
+      return this.$store.state.profile.picture;
     },
   },
 
@@ -50,5 +67,32 @@ export default {
 
   &__item
     text-transform uppercase
+
+  &__image-aspect
+    width 40px
+    height 40px
+    position relative
+    margin 0 10px
+
+  &__image-crop
+    width 100%
+    border-radius 100%
+    height auto
+    position absolute
+    top 0
+    bottom 0
+    left 0
+    right 0
+    overflow hidden
+    text-align center
+    display flex
+    justify-content center
+    cursor pointer
+
+  &__image
+    min-width 100%
+    min-height 100%
+    height auto
+    margin 0 auto
 
 </style>
