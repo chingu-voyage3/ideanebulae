2d1cf63d5e4637a49dcf936a7b1377a135cd657f
Oxyrus <im.oxyrus@gmail.com>
2017-11-06 11:01:19 -0500

    Uses the user _id to create the idea

--- server/router/index.js
+++ server/router/index.js
@@ -4,6 +4,7 @@ import config from '../services/mongoose.config';
 import User from '../models/user';
 import Idea from '../models/idea';
 import authCheck from '../utils/authCheck';
+import decodeToken from '../utils/decodeToken';
 
 const router = Router();
 
@@ -59,15 +60,37 @@ router.route('/ideas')
       .catch(err => res.send(err));
   })
   .post((req, res) => {
-    Idea.saveIdea(req.body)
-      .then(() => {
-        console.log('Idea created');
-        res.json('Idea created');
+    // When saving an idea we also need to find the user
+    // who is trying to create it, so we can stablish the
+    // relation using the user _id in the idea object
+    // by doing so, we can then use populate() to get the
+    // idea with the user as well
+
+    // First we get the token from the headers
+    let token = req.headers['authorization'].slice(7);
+
+    // Then we decode it and extract sub only
+    let { sub } = decodeToken(token);
+
+    // Now we try to find a using with a matching sub or user_id
+    User.findUserBySub(sub)
+      // If there's an user, we can use its _id to create the idea
+      .then((user) => {
+        Idea.saveIdea(req.body, user._id)
+          .then(() => {
+            console.log('Idea created');
+            res.json('Idea created');
+          })
+          .catch((err) => {
+            console.error(err);
+            res.send(err)
+          })
       })
-      .catch(err => {
+      // There's no user, don't do anything
+      .catch((err) => {
         console.error(err);
         res.send(err);
-      });
+      })
   })
 
 router.route('/ideas/search/:searchForTags(*):searchForKeywords(*)')
@@ -78,7 +101,7 @@ router.route('/ideas/search/:searchForTags(*):searchForKeywords(*)')
       })
       .catch(err => res.send(err));
   });
-  
+
 router.route('/ideas/getAllTags')
   .get((req, res) => {
     Idea.getAllTags()
