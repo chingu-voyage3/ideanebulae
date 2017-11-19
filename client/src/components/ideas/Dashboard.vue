0300b4227bce75e223a34a4b6ff7b0140822b141
Parminder Singh <parmsingh129@gmail.com>
2017-11-19 16:23:57 +0530

    Added hover's working

--- client/src/components/ideas/Dashboard.vue
+++ client/src/components/ideas/Dashboard.vue
@@ -11,24 +11,35 @@
           </div>
           <div class="dashboard__idealist-right">
             <div class="dashboard__idealist-reviewcount">{{ idea.reviews.length }}</div>
-            <div class="dashboard__idealist-quicklook"><img src="../../assets/eye-sym.svg" height="10" width="10"></div>
+            <div class="dashboard__idealist-quicklook" @mouseover="hoverOver=index" @mouseout="hoverOver=-1"><img src="../../assets/eye-sym.svg" height="10" width="10"></div>
           </div>
         </li>
       </ul>
+      <div class="dashboard__idealist-hover" v-if="hoverOver!=-1">
+        <ul class="dashboard__idealist-hover-list">
+          <li class="dashboard__idealist-hover-review" v-bind:key="index" v-for="(review, index) in ideas[hoverOver].reviews">
+            <div class="dashboard__idealist-hover-review-text">{{ review.comments | truncate(70) }}</div>
+            <div class="dashboard__idealist-hover-review-id">{{ review.reviewer }}</div>
+          </li>
+        </ul>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
 import http from '../../api/index';
+import filters from '../../filters';
 
 export default {
   name: 'Dashboard',
   data() {
     return {
       ideas: [],
+      hoverOver: -1,
     };
   },
+  filters,
   mounted() {
     http.get('/ideas')
     .then((response) => {
