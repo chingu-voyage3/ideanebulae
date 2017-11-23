e5b1c8c678276076e3f8b4db5b905c994d8f553c
Jim Medlock <jdmedlock@gmail.com>
2017-11-22 21:58:18 -0600

    Update for production deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,8 +4,11 @@ import { getAccessToken } from '@/auth';
 // Create http object to send the access token
 // along with every single request
 /* eslint-disable */
+const port = process.env.PORT || 7000;
+const apiURL = `http://localhost:${port}/api`;
+console.log('API URL:', apiURL);
 const http = new axios.create({
-  baseURL: 'http://localhost:7000/api',
+  baseURL: apiURL,
   timeout: 5000,
   headers: {
     Authorization: `Bearer ${getAccessToken()}`,
