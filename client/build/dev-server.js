282f30f76fa7a63f9c93e216ea3569dfd25dcd78
Jim Medlock <jdmedlock@gmail.com>
2017-11-24 12:02:59 -0600

    Add console.logs for Heroku debugging

--- client/build/dev-server.js
+++ client/build/dev-server.js
@@ -1,7 +1,7 @@
 require('./check-versions')()
 
 var config = require('../config')
-console.log('NODE_ENV: ', NODE_ENV);
+console.log('NODE_ENV: ', process.env.NODE_ENV);
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
