60628bdcbc1da40ea531ab7fb56d0bd3890bba6b
Jim Medlock <jdmedlock@gmail.com>
2017-11-24 19:43:29 -0600

    Deploy to Heroku

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,3 +1,5 @@
 module.exports = {
-  NODE_ENV: '"production"'
-}
+  NODE_ENV: '"production"',
+  API_HOST: '',
+  API_PORT: '',
+};
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,22 +7,20 @@ import { getAccessToken } from '@/auth';
 
 // TODO: Determine why the following doesn't work. The only variable in process.env
 // is NODE_ENV. All other config variables are undefined.
-/*
+
 console.log('API_HOST: ', process.env.API_HOST, ' API_PORT: ',process.env.API_PORT);
 const apiHost = process.env.API_HOST;
 const apiPort = (process.env.API_PORT) ? process.env.API_PORT : null;
 console.log(`apiHost: ${apiHost} apiPort: ${apiPort}`);
 console.log('process.env: ', process.env);
 let apiURL = '';
-if (apiHost) {
-  apiURL = (apiPort) ? `${apiHost}:${apiPort}` : apiHost;
+if (apiHost !== '') {
+  apiURL = (apiPort !== '') ? `${apiHost}:${apiPort}` : apiHost;
 } else {
   apiURL = 'http://localhost:7000/api';
 }
 console.log(`apiHost: ${apiHost} apiPort: ${apiPort} resulting apiURL: ${apiURL}`);
-*/
 
-const apiURL = 'http://ideanebulae.herokuapp.com';
 const http = new axios.create({
   baseURL: apiURL,
   timeout: 5000,
