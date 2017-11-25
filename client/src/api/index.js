9854a6fdeba6dea1d61efd4b60ddbc27c497ae54
Jim Medlock <jdmedlock@gmail.com>
2017-11-25 08:03:47 -0600

    Move process.env console logging

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,26 +4,6 @@ import { getAccessToken } from '@/auth';
 // Create http object to send the access token
 // along with every single request
 /* eslint-disable */
-
-// TODO: Determine why the following doesn't work. The only variable in process.env
-// is NODE_ENV. All other config variables are undefined.
-
-console.log('API_HOST: ', process.env.API_HOST, ' API_PORT: ',process.env.API_PORT);
-console.log('process.env: ', process.env);
-/*
-const apiHost = process.env.API_HOST;
-const apiPort = (process.env.API_PORT) ? process.env.API_PORT : null;
-console.log(`apiHost: ${apiHost} apiPort: ${apiPort}`);
-console.log('process.env: ', process.env);
-let apiURL = '';
-if (apiHost !== '') {
-  apiURL = (apiPort !== '') ? `${apiHost}:${apiPort}` : apiHost;
-} else {
-  apiURL = 'http://localhost:7000/api';
-}
-console.log(`apiHost: ${apiHost} apiPort: ${apiPort} resulting apiURL: ${apiURL}`);
-*/
-
 const apiURL = 'http://ideanebulaeas.herokuapp.com/api';
 const http = new axios.create({
   baseURL: apiURL,
