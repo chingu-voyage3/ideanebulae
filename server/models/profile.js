c10457817b083325a23b92a8f3f29ee317e0f4a0
Jim Medlock <jdmedlock@gmail.com>
2017-12-16 22:44:52 -0600

    Modify models to support Sequelize

--- server/models/profile.js
+++ server/models/profile.js
@@ -5,52 +5,56 @@ import Idea from "./idea";
 import Agreement from "./agreement";
 import Review from "./review";
 
-const sequelize = db.get();
+let Profile = null;
 
-const Profile = sequelize.define(
-  "profile",
-  {
-    user_id: {
-      type: Sequelize.STRING,
-      primaryKey: true
+const getProfile = () => {
+  Profile = db.get().define(
+    "profile",
+    {
+      user_id: {
+        type: Sequelize.STRING,
+        primaryKey: true
+      },
+
+      // User name used for authentication (Auth0 nickname field)
+      username: {
+        type: Sequelize.STRING
+      },
+
+      // User full name (Auth0 name field)
+      name: {
+        type: Sequelize.STRING
+      },
+
+      // User's social media avatar (Auth0 picture field)
+      avatar_url: {
+        type: Sequelize.STRING
+      },
+
+      // User's qualification description
+      qualifications: {
+        type: Sequelize.STRING
+      }
     },
-
-    // User name used for authentication (Auth0 nickname field)
-    username: {
-      type: Sequelize.STRING
-    },
-
-    // User full name (Auth0 name field)
-    name: {
-      type: Sequelize.STRING
-    },
-
-    // User's social media avatar (Auth0 picture field)
-    avatar_url: {
-      type: Sequelize.STRING
-    },
-
-    // User's qualification description
-    qualifications: {
-      type: Sequelize.STRING
+    {
+      // By default, sequelize automatically pluralizes table names unless
+      // this option is enabled.
+      freezeTableName: true,
+      // By default, sequelize generates columns such as 'createdAt' and
+      // 'modifiedAt' which are automatically timestamped unless this
+      // option is disabled.
+      timestamps: false,
+      // The default casing is camelCase however if the source model is
+      // configured with underscored: true the foreignKey will be snake_case.
+      underscored: true
     }
-  },
-  {
-    // By default, sequelize automatically pluralizes table names unless
-    // this option is enabled.
-    freezeTableName: true,
-    // By default, sequelize generates columns such as 'createdAt' and
-    // 'modifiedAt' which are automatically timestamped unless this
-    // option is disabled.
-    timestamps: false,
-    // The default casing is camelCase however if the source model is
-    // configured with underscored: true the foreignKey will be snake_case.
-    underscored: true
-  }
-);
+  );
+};
 
-Profile.hasMany(Idea);
-Profile.hasMany(Agreement);
-Profile.hasMany(Review);
+const defineProfileRelations = () => {
+  db.get().hasMany(Idea);
+  db.get().hasMany(Agreement);
+  db.get().hasMany(Review);
+};
 
-export default Profile;
+export { Profile, getProfile, defineProfileRelations };
