43b16c88f28acc1c0671ca92fae0472bb2635aac
Oxyrus <im.oxyrus@gmail.com>
2017-12-18 17:52:53 -0500

    Initialize sequelize

---
+++ db/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'postgres',
+    database: 'ideanebulae_dev',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    define: {
+      underscored: true, // snake_case timestamps
+      underscoredAll: true, // Converts camelCase model names to snake_case
+    },
+  },
+  test: {
+    username: 'postgres',
+    password: 'postgres',
+    database: 'ideanebulae_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    define: {
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
+    define: {
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+}
