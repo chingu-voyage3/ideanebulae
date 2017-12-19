f1c973ca93b9b07cc9e3f6a3dc8cd5d43ffe8a45
Oxyrus <im.oxyrus@gmail.com>
2017-12-18 21:42:38 -0500

    Generate simple sequelize models & migrations

--- db/migrations/20171219012348-create-idea.js
+++ db/migrations/20171219012348-create-idea.js
@@ -6,7 +6,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
         type: Sequelize.STRING,
