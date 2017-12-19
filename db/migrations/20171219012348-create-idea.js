1a268a19568585d9de66a7dbfb71adf547055542
Oxyrus <im.oxyrus@gmail.com>
2017-12-18 20:45:09 -0500

    Generate idea model & migration

---
+++ db/migrations/20171219012348-create-idea.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Ideas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      idea_type: {
+        type: Sequelize.ENUM,
+        values: ['public', 'private', 'commercial'],
+        defaultValue: 'public',
+        allowNull: false,
+      },
+      profile_id: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Profiles',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Ideas');
+  }
+};
