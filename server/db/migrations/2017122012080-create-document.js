8743a45a1716a311a9381615a7a4eac8ee9d0a11
Jim Medlock <jdmedlock@gmail.com>
2017-12-20 12:30:02 -0600

    Add 'document' table to contain supporting idea information

---
+++ server/db/migrations/2017122012080-create-document.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('documents', {
+      idea_id: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'ideas',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      profile_id: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'profiles',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      idea_type: {
+        type: Sequelize.ENUM,
+        values: ['public', 'private', 'commercial'],
+        defaultValue: 'public',
+        allowNull: false,
+      },
+      url: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('documents');
+  }
+};
---
+++ server/db/models/document.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Document = sequelize.define('document', {
+    idea_id: {
+      type: DataTypes.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'ideas',
+        key: 'id',
+      },
+      allowNull: false,
+    },
+
+    profile_id: {
+      type: DataTypes.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'profiles',
+        key: 'id',
+      },
+      allowNull: false,
+    },
+
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    idea_type: {
+      type: DataTypes.ENUM,
+      values: ['public', 'private', 'commercial'],
+      defaultValue: 'public',
+      allowNull: false,
+    },
+
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+  }, {
+      underscored: true,
+      classMethods: {
+        associate: function (models) {
+          // associations can be defined here
+          Document.belongsTo(models.Profile);
+          Document.belongsTo(models.Idea);
+        }
+      }
+    });
+  return Document;
+};
