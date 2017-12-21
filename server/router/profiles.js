5618fbbfa3685b6658c650dde9a5357b10378fdd
Oxyrus <im.oxyrus@gmail.com>
2017-12-21 15:06:47 -0500

    Finish profiles endpoint migration

--- server/router/profiles.js
+++ server/router/profiles.js
@@ -20,14 +20,10 @@ router.get('/profiles', async (req, res) => {
  * @param {object} res - The response object
  * @return {object} The profile found in the database
  */
-router.get('/profile/:username(*)', (req, res) => {
-  models.Profile.findOne({ where: { username: req.params.username } })
-    .then((profile) => {
-      res.json(profile);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+router.get('/profile/:username(*)', async (req, res) => {
+  const { username } = req.params;
+  const profile = await models.Profile.findOne({ where: { username } });
+  res.json(profile);
 });
 
 /**
@@ -36,7 +32,7 @@ router.get('/profile/:username(*)', (req, res) => {
  * @param {object} res - The response object
  * @return {object} The created/updated profile
  */
-router.put('/profile/:userId(*)', (req, res) => {
+router.put('/profile/:userId(*)', async (req, res) => {
   const { user_id, username, name, avatar_url, qualifications } = req.body;
   /**
    * First we need to check if there's a profile for the given user_id
@@ -44,41 +40,39 @@ router.put('/profile/:userId(*)', (req, res) => {
    * however if it doesn't exist, we want to create one
    */
 
-  models.Profile.findOne({ where: { user_id } })
-    .then((profile) => {
-      if (profile) {
-        profile.update({
-          username,
-          name,
-          avatar_url,
-          qualifications,
-        })
-          .then((updatedProfile) => {
-            res.json(updatedProfile);
-          })
-          .catch((err) => {
-            res.json(err);
-          });
-      }
-      else {
-        models.Profile.create({
-          user_id,
-          username,
-          name,
-          avatar_url,
-          qualifications,
-        })
-          .then((createdProfile) => {
-            res.json(createdProfile);
-          })
-          .catch((err) => {
-            res.json(err);
-          });
-      }
+  const profile = await models.Profile.findOne({ where: { user_id } });
+
+  // We found a profile, therefore we need to update it
+  if (profile) {
+    profile.update({
+      username,
+      name,
+      avatar_url,
+      qualifications,
     })
-    .catch((err) => {
-      res.json(err);
-    });
+      .then((updatedProfile) => {
+        res.json(updatedProfile);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  }
+  // We didn't find a profile, so it's time to create one
+  else {
+    models.Profile.create({
+      user_id,
+      username,
+      name,
+      avatar_url,
+      qualifications,
+    })
+      .then((createdProfile) => {
+        res.json(createdProfile);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  }
 });
 
 module.exports = router;
