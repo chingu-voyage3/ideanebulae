f93360102db2eb1992f7ad9faaf7de2c19830a95
Jim Medlock <jdmedlock@gmail.com>
2018-01-13 16:49:49 -0600

    Correct database environment variables to use correct variable names

--- server/db/config.js
+++ server/db/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   development: {
-    username: process.env.DATABASE_USER || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'postgres',
-    database: process.env.DATABASE_NAME || 'ideanebulae_dev',
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    port: process.env.DATABASE_PORT || 5432,
+    username: process.env.DBUSERID,
+    password: process.env.DBPASSWD,
+    database: process.env.DBNAME,
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
     dialect: 'postgres',
     logging: console.log,
     define: {
@@ -13,11 +13,11 @@ module.exports = {
     },
   },
   test: {
-    username: process.env.DATABASE_USER || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'postgres',
-    database: process.env.DATABASE_NAME || 'ideanebulae_test',
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    port: process.env.DATABASE_PORT || 5432,
+    username: process.env.DBUSERID,
+    password: process.env.DBPASSWD,
+    database: process.env.DBNAME,
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
     dialect: 'postgres',
     define: {
       underscored: true,
@@ -25,11 +25,11 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
+    username: process.env.DBUSERID,
+    password: process.env.DBPASSWD,
+    database: process.env.DBNAME,
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
     dialect: 'postgres',
     define: {
       underscored: true,
